f2d018c5b51f4284660dcd7902f0ecffeb4ee8f0
JustClips <135924737+JustClips@users.noreply.github.com>
2025-07-12 12:14:49 -0500

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -5,35 +5,15 @@ local function loadModule(path)
     return assert(loadstring(src))()
 end
 
--- 1) Load UI library
-local UILib = loadModule("modules/ui.lua")
+-- 1) Load & run the UI-lib (it creates the ScreenGui, mainFrame, sidebar & tabSections)
+local UI = loadModule("modules/ui.lua")
 
--- 2) Create main window
-local Window = UILib:NewWindow({
-    Title = "Eps1llon Hub 2025",
-    Size  = UDim2.fromOffset(600, 500),
-})
-
--- 3) Grab sections
-local sections = {
-    Configuration = UILib:GetSection("Configuration"),
-    Combat        = UILib:GetSection("Combat"),
-    ESP           = UILib:GetSection("ESP"),
-    Inventory     = UILib:GetSection("Inventory"),
-    Misc          = UILib:GetSection("Misc"),
-    ["UI Settings"]= UILib:GetSection("UI Settings"),
-}
-
--- 4) Wire up feature modules
-
--- Combat (uses SetupTab)
-local Aimbot = loadModule("modules/aimbot.lua")
-Aimbot:SetupTab(sections.Combat)
-
--- ESP (uses Setup and takes the UI-lib)
+-- 2) Wire up your ESP module (it now uses UI:AddToggle under the hood)
 local ESP = loadModule("modules/esp.lua")
-ESP:Setup(UILib)
+ESP:Setup(UI)
 
--- Misc / Utilities (uses SetupTab)
-local Utils = loadModule("modules/utils.lua")
-Utils:SetupTab(sections.Misc)
+-- 3) (Optional) Wire up other modules the same way, passing the UI instance:
+-- local Aimbot = loadModule("modules/aimbot.lua")
+-- Aimbot:Setup(UI)
+-- local Utils   = loadModule("modules/utils.lua")
+-- Utils:Setup(UI)
